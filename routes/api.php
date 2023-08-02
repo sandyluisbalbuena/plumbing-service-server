6880db13ef4d8f966e90ef00dde484a8320aa5c5
sandyluisbalbuena <balbuenasandyluis12@gmail.com>
2023-08-03 00:44:46 +0800

    wew

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\InquiryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,11 +15,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
-
-
-
-
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+// Route::group(['middleware' => 'bearer_auth'], function () {
+    Route::get('inquiry', [InquiryController::class, 'index']);
+// });
