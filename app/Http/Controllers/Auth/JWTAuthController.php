daf793d8485d41a7c76c8b8a63e6477601439a1a
sandyluisbalbuena <balbuenasandyluis12@gmail.com>
2023-08-03 13:53:09 +0800

    wew

--- app/Http/Controllers/Auth/JWTAuthController.php
+++ app/Http/Controllers/Auth/JWTAuthController.php
@@ -40,7 +40,7 @@ class JWTAuthController extends Controller
         }
 
         // Set the token's expiration time (e.g., 1 hour from now)
-        $expiration = Carbon::now()->addHour()->timestamp;
+        $expiration = Carbon::now()->addMinute()->timestamp;
 
         // Retrieve the user from the database
         $user = User::where('email', $credentials['email'])->first();
