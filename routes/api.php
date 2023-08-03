d366ea32fbcce145cc25d348f25d7b638c3e56c8
sandyluisbalbuena <balbuenasandyluis12@gmail.com>
2023-08-04 05:00:09 +0800

    wew

--- routes/api.php
+++ routes/api.php
@@ -8,17 +8,17 @@ use App\Http\Controllers\ServiceController;
 
 Route::middleware('jwt.auth')->group(function () {
     // inquiryRefs Routes
-    Route::get('inquiry', [InquiryController::class, 'getInquiries']);
-    Route::post('inquiry', [InquiryController::class, 'postInquiry']);
-    Route::post('inquiry/{inquiryId}', [InquiryController::class, 'putInquiry']);
-    Route::delete('inquiry/{inquiryId}', [InquiryController::class, 'deleteInquiry']);
+    Route::get('inquiries', [InquiryController::class, 'getInquiries']);
+    Route::post('inquiries', [InquiryController::class, 'postInquiry']);
+    Route::post('inquiries/{inquiryId}', [InquiryController::class, 'putInquiry']);
+    Route::delete('inquiries/{inquiryId}', [InquiryController::class, 'deleteInquiry']);
 
 
     // serviceRefs Routes
-    Route::get('service', [ServiceController::class, 'getServices']);
-    Route::post('service', [ServiceController::class, 'postService']);
-    Route::post('service/{serviceId}', [ServiceController::class, 'putService']);
-    Route::delete('service/{serviceId}', [ServiceController::class, 'deleteService']);
+    Route::get('services', [ServiceController::class, 'getServices']);
+    Route::post('services', [ServiceController::class, 'postService']);
+    Route::post('services/{serviceId}', [ServiceController::class, 'putService']);
+    Route::delete('services/{serviceId}', [ServiceController::class, 'deleteService']);
 });
 
 Route::get('generate-token', [JWTAuthController::class, 'generateToken']);
