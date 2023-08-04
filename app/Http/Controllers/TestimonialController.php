7984c5220788ffbe22fbf1d4b57aafe7496a5b0c
sandyluisbalbuena <balbuenasandyluis12@gmail.com>
2023-08-05 02:00:48 +0800

    wew

---
+++ app/Http/Controllers/TestimonialController.php
@@ -0,0 +1,137 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Kreait\Laravel\Firebase\Facades\Firebase;
+
+class TestimonialController extends Controller
+{
+    public function __construct()
+    {
+        $this->database = Firebase::database();
+        $this->testimonials = 'testimonials';
+    }
+
+    public function index()
+    {
+        return response()->json('wew');
+    }
+
+    public function getTestimonials()
+    {
+        $services = $this->database->getReference($this->testimonials)->getValue();
+
+        return response()->json(compact('services'), 200);
+    }
+
+    public function getTestimonial($testimonialId)
+    {
+        $service = $this->database->getReference($this->testimonials.'/'.$testimonialId)->getValue();
+
+        return response()->json(compact('service'), 200);
+    }
+
+    public function postTestimonial(Request $request)
+    {
+        $requiredFields = ['name'];
+
+        foreach ($requiredFields as $field) {
+            if (!$request->has($field) || empty($request->input($field))) {
+                return response()->json(['message' => "The field '{$field}' is required."], 400);
+            }
+        }
+
+        // $url = 'api/thread';
+        // $method = 'post';
+
+        // $postData = [
+        //     'categoryId' => $request->categoryId,
+        //     'title' => $request->title,
+        //     'slug' => $request->slug,
+        //     'content' => $request->content,
+        //     'userId' => $request->userId,
+        //     'createdAt' => $request->createdAt,
+        //     'updatedAt' => $request->updatedAt,
+        // ];
+
+        $postData = [
+            'name' => $request->name,
+            'createdAt' => time(),
+            'updatedAt' => time(),
+        ];
+
+        // Save the data to the 'threads' reference
+        // $postData = [
+        //     'title' => 'sample title',
+        //     'slug' => 'sample slug',
+        //     'content' => 'sample content',
+        //     'createdAt' => time(),
+        //     'updatedAt' => time(),
+        // ];
+
+        $service = $this->database->getReference($this->testimonials)->push($postData);
+
+        // Save the data to the 'apiHistory' reference
+        // $historyRef = $this->database->getReference($this->apiHistorytable)->push($postDataHistory);
+
+        if ($service) {
+            return response()->json('success', 200);
+        } else {
+            return response()->json('fail', 500);
+        }
+    }
+
+    public function putTestimonial(Request $request, $testimonialId)
+    {
+        $requiredFields = ['name'];
+
+        // return response()->json($request, 200);
+
+
+        foreach ($requiredFields as $field) {
+            if (!$request->has($field) || empty($request->input($field))) {
+                return response()->json(['message' => "The field '{$field}' is required."], 400);
+            }
+        }
+
+        // $url = 'api/thread';
+        // $method = 'post';
+
+        // $postData = [
+        //     'categoryId' => $request->categoryId,
+        //     'title' => $request->title,
+        //     'slug' => $request->slug,
+        //     'content' => $request->content,
+        //     'userId' => $request->userId,
+        //     'createdAt' => $request->createdAt,
+        //     'updatedAt' => $request->updatedAt,
+        // ];
+
+        $postData = [
+            'name' => $request->name,
+            'updatedAt' => time(),
+        ];
+
+
+        $service = $this->database->getReference($this->testimonials.'/'.$testimonialId)->update($postData);
+
+
+        if ($service) {
+            return response()->json('success', 200);
+        } else {
+            return response()->json('fail', 500);
+        }
+    }
+
+    public function deleteTestimonial($testimonialId)
+    {
+        $service = $this->database->getReference($this->testimonials.'/'.$testimonialId)->remove();
+
+        if ($service) {
+            return response()->json('success', 200);
+        } else {
+            return response()->json('fail', 500);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\InquiryController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\JWTAuthController;
 use App\Http\Controllers\ServiceController;
+use App\Http\Controllers\TestimonialController;
 
 Route::middleware('jwt.auth')->group(function () {
     // inquiryRefs Routes
@@ -12,6 +13,11 @@ Route::middleware('jwt.auth')->group(function () {
     Route::post('inquiries/{inquiryId}', [InquiryController::class, 'putInquiry']);
     Route::delete('inquiries/{inquiryId}', [InquiryController::class, 'deleteInquiry']);
 
+    // testimonials Routes
+    // Route::get('testimonials', [TestimonialController::class, 'getTestimonials']);
+    // Route::post('testimonials', [TestimonialController::class, 'postTestimonials']);
+    Route::post('testimonials/{testimonialId}', [TestimonialController::class, 'putTestimonial']);
+    Route::delete('testimonials/{testimonialId}', [TestimonialController::class, 'deleteTestimonial']);
 
     // serviceCategoryRefs Routes
     Route::get('servicecategories', [ServiceController::class, 'getServiceCategories']);
@@ -19,8 +25,10 @@ Route::middleware('jwt.auth')->group(function () {
     Route::get('servicecategories/{serviceId}', [ServiceController::class, 'getServiceCategory']);
     Route::post('servicecategories/{serviceId}', [ServiceController::class, 'putServiceCategory']);
     Route::delete('servicecategories/{serviceId}', [ServiceController::class, 'deleteServiceCategory']);
-
-
 });
 
 Route::post('generate-token', [JWTAuthController::class, 'generateToken']);
+
+
+Route::get('testimonials', [TestimonialController::class, 'getTestimonials']);
+Route::post('testimonials', [TestimonialController::class, 'postTestimonial']);
